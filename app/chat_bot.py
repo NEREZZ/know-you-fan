2d4c29e2d8e82cbb147a9119274d69cce42b29c0
Pedro Neres <115440600+NEREZZ@users.noreply.github.com>
2025-05-04 23:57:25 -0300

    Update chat_bot.py
    retirado chave da API que estava exposta

--- app/chat_bot.py
+++ app/chat_bot.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyDQRH7pyk1cQ6n6IlPPb7gjicrL4z9ipDk")
+client = genai.Client(api_key="")
 def bot(pergunta):
     response = client.models.generate_content(
         model="gemini-2.0-flash",
